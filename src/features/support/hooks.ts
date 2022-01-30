7150b60723a44899b646a2981705d44bee6060e7
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2022-01-30 17:49:36 +0000

    inject config from ci

--- src/features/support/hooks.ts
+++ src/features/support/hooks.ts
@@ -1,8 +1,9 @@
 import { CoinRanking } from "../world/coin-ranking"
 import { Before, BeforeAll, AfterAll } from "cucumber";
+import env from "../../config/env.json";
 
 Before(function (this: CoinRanking) {
-
+    console.log("base url is:" + env.BASE_URL);
 });
 
 BeforeAll(function () {
